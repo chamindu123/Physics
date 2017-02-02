3c702c46c967f5ff285e3b6ece1d0bf05ef32077
chamindu123 <chaminduamarasinghe@gmail.com>
2017-02-02 17:38:24 -0600

    Add files via upload

---
+++ MadelungSquare.py
@@ -0,0 +1,40 @@
+import math
+import pylab as plt #For plotting
+
+globalMD = 1.61554262658 #Calculated for a square of side 2000
+
+def main():
+    L = percentage_list(list_of_MD()) #Generates a list of the percentages
+
+    #-- Plotting routine --#
+    plt.plot(L[2:len(L)]) 
+    plt.ylabel('Percentage deviation from precise Madelung constant')
+    plt.xlabel('Length of side of square')
+    plt.show()
+    #----------------------#
+
+def dist( x,y ): #Calculates the distance of (x,y) from (0,0)
+    return math.sqrt((x*x)+(y*y))
+
+def ML(n): #Calculates the madelung constant for a square lattice of side 2*n
+    mdlung = 0
+    for i in range(-n,n): #Loops through x values
+        for j in range(-n,n): #Loops through y values
+            if i != 0 or j != 0: #Ignores the center point
+                mdlung += pow(-1,1+i+j)/dist(i,j) #Adds contribution from each point
+    return mdlung
+
+def list_of_MD(): #Returns a list of Madelung constants for a range of n 
+    L = []
+    for n in range(2,50):
+        L.append(ML(n/2))
+    return L
+
+def percentage_list(L): #Uses list of constants to return a list of percentage deviations
+    P = []
+    for i in L:
+        P.append(100*(globalMD-i)/globalMD)
+    return P
+
+if __name__ == "__main__":
+    main()
